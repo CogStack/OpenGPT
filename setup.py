63e22cedf12878bd050e2e82d9f1e874b6b0b3cb
w-is-h <w.kraljevic@gmail.com>
2023-05-10 13:17:20 +0100

    version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("./README.md", "r") as fh:
 
 setuptools.setup(
     name="opengpt",
-    version="0.0.4",
+    version="0.0.5",
     author="w-is-h",
     author_email="w.kraljevic@gmail.com",
     description="OpenGPT a framework for producing grounded domain specific LLMs, and NHS-LLM a conversational model for healthcare made using OpenGPT.",
